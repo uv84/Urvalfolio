5b2c2bb799aa79e438210f5ae41466eaeafc4db3
uv84 <urvalchikhale@gmail.com>
2025-11-19 03:18:01 +0530

    removed errors

--- src/app/(app)/(root)/page.tsx
+++ src/app/(app)/(root)/page.tsx
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 import type { ProfilePage as PageSchema, WithContext } from "schema-dts";
+
 import { About } from "@/features/profile/components/about";
 import { Certifications } from "@/features/profile/components/certifications";
 import { Experiences } from "@/features/profile/components/experiences";
